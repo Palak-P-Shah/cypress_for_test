9c724c3e8b4f99c76b0592fc329bf9ca66369cb3
Palak P Shah <dabbadolcha@gmail.com>
2024-01-14 00:00:17 +0530

    changes test file

--- cypress/e2e/test/testSample_Ham_Menu.cy.js
+++ cypress/e2e/test/testSample_Ham_Menu.cy.js
@@ -3,21 +3,22 @@ describe('Test Suite to check links of Hamburger Menu' , function() {
         // cy.viewport(393, 852);
         cy.viewport(1920, 1080);
         cy.visit('https://staging.blavity.com/')
-        Cypress.on('uncaught:exception', (err, runnable) => { return false; })
-        cy.get('#nav-menu-toggler > svg')
-        .click()
-        // for homeand texture link in hamburger menu
-        cy.get('[class="gap-y-2 grid grid-cols-1"]')
-            .find('li a')
-            .eq(0)
-            //.should('have.attr', 'target')
-            .invoke('removeAttr', 'target')
-            .click()
-            cy.wait(4000)
-            //Home & Texture - Home & Texture
-            cy.title().should('eq','Home & Texture - Home & Texture')
-            cy.go('back')
-            cy.title().should('eq','The Community for Black Creativity and News - Blavity News')
+        cy.title().should('eq','The Community for Black Creativity and News - Blavity News')
+        // Cypress.on('uncaught:exception', (err, runnable) => { return false; })
+        // cy.get('#nav-menu-toggler > svg')
+        // .click()
+        // // for homeand texture link in hamburger menu
+        // cy.get('[class="gap-y-2 grid grid-cols-1"]')
+        //     .find('li a')
+        //     .eq(0)
+        //     //.should('have.attr', 'target')
+        //     .invoke('removeAttr', 'target')
+        //     .click()
+        //     cy.wait(4000)
+        //     //Home & Texture - Home & Texture
+        //     cy.title().should('eq','Home & Texture - Home & Texture')
+        //     cy.go('back')
+        //     cy.title().should('eq','The Community for Black Creativity and News - Blavity News')
         
     })
     it("check if Afrotech link is working or not in hamburger menu", function () {
