9ae5bcd008a65f93453212953bfb982b78ac7ef1
Palak P Shah <dabbadolcha@gmail.com>
2024-01-13 21:30:53 +0530

    initial commit

---
+++ cypress/e2e/test/mobile_HorrizontalBar_Present.spec.js
@@ -0,0 +1,50 @@
+
+
+describe('Mobile :- Test Suite to check whether Horrizontal bar is presnet', () => {
+    it('Mobile :- check whether webpage is having horrizontal scroll bar or not - 1', () => {
+      cy.viewport(393, 852);
+      // cy.viewport(1920, 1080);
+      cy.visit('https://staging.blavity.com/russell-westbrook-hbcu-pensole-lewis-lounge');
+      Cypress.on('uncaught:exception', (err, runnable) => { return false; })
+      cy.window().then(win => {
+        const htmlWidth = Cypress.$('html')[0].scrollWidth;
+        const scrollBarWidth = win.innerWidth - htmlWidth;
+        cy.log(htmlWidth)
+        cy.log(scrollBarWidth)
+        expect(scrollBarWidth).to.be.gt(0);                 // scrollbar is present
+        // if fails Horrizontal scrollBar is present in the Mobile View
+      })
+      
+    })
+    it('Mobile :- check whether webpage is having horrizontal scroll bar or not - 2', () => {
+      cy.viewport(393, 852);
+      cy.visit('https://blavity.com/russell-westbrook-hbcu-pensole-lewis-lounge');
+      Cypress.on('uncaught:exception', (err, runnable) => { return false; })
+      cy.window().then(win => {
+        const htmlWidth = Cypress.$('html')[0].scrollWidth;
+        const scrollBarWidth = win.innerWidth - htmlWidth;
+        cy.log(htmlWidth)
+        cy.log(scrollBarWidth)
+        expect(scrollBarWidth).to.be.gt(0);                 // scrollbar is present
+        // if fails Horrizontal scrollBar is present in the Mobile View
+      })
+    })
+    it('Mobile :- check whether webpage is having horrizontal scroll bar or not - 3', () => {
+      cy.viewport(393, 852);
+      cy.visit('https://staging.blavity.com/entertainment/whoopi-goldberg-the-color-purple-2023-scene');
+      Cypress.on('uncaught:exception', (err, runnable) => { return false; })
+      cy.window().then(win => {
+        const htmlWidth = Cypress.$('html')[0].scrollWidth;
+        const scrollBarWidth = win.innerWidth - htmlWidth;
+        cy.log(htmlWidth)
+        cy.log(scrollBarWidth)
+        expect(scrollBarWidth).to.be.gt(0);                 // scrollbar is present
+        // if fails Horrizontal scrollBar is present in the Mobile View
+      })
+    })
+});
+
+
+
+
+
