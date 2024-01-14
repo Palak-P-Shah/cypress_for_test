8c905079730f1e808d98698776803f14a8131597
Palak P Shah <dabbadolcha@gmail.com>
2024-01-14 19:37:59 +0530

    changed config file for allure

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   chromeWebSecurity: false,
   // modifyObstructiveCode:false,
-    
+  "env": { 
+    "allureResultsPath": "allure-results",
+  },  
   e2e: {
     supportFile: false,
     setupNodeEvents(on, config) {
