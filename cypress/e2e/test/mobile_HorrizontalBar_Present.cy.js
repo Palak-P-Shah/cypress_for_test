94b8dede601ece29ebeed95311889a19d7e438d7
Palak P Shah <dabbadolcha@gmail.com>
2024-01-24 23:04:45 +0530

    added urls in mobile_hz_bar

--- cypress/e2e/test/mobile_HorrizontalBar_Present.cy.js
+++ cypress/e2e/test/mobile_HorrizontalBar_Present.cy.js
@@ -1,7 +1,7 @@
 
 
 describe('Mobile :- Test Suite to check whether Horrizontal bar is presnet', () => {
-    it('Mobile :- check whether webpage is having horrizontal scroll bar or not - 1', () => {
+    it('Mobile :- check whether webpage is having horrizontal scroll bar or not for https://staging.blavity.com/russell-westbrook-hbcu-pensole-lewis-lounge', () => {
       cy.viewport(393, 852);
       // cy.viewport(1920, 1080);
       cy.visit('https://staging.blavity.com/russell-westbrook-hbcu-pensole-lewis-lounge');
@@ -16,7 +16,7 @@ describe('Mobile :- Test Suite to check whether Horrizontal bar is presnet', () 
       })
       
     })
-    it('Mobile :- check whether webpage is having horrizontal scroll bar or not - 2', () => {
+    it('Mobile :- check whether webpage is having horrizontal scroll bar or not for https://blavity.com/russell-westbrook-hbcu-pensole-lewis-lounge', () => {
       cy.viewport(393, 852);
       cy.visit('https://blavity.com/russell-westbrook-hbcu-pensole-lewis-lounge');
       Cypress.on('uncaught:exception', (err, runnable) => { return false; })
@@ -29,7 +29,7 @@ describe('Mobile :- Test Suite to check whether Horrizontal bar is presnet', () 
         // if fails Horrizontal scrollBar is present in the Mobile View
       })
     })
-    it('Mobile :- check whether webpage is having horrizontal scroll bar or not - 3', () => {
+    it('Mobile :- check whether webpage is having horrizontal scroll bar or not for https://staging.blavity.com/entertainment/whoopi-goldberg-the-color-purple-2023-scene', () => {
       cy.viewport(393, 852);
       cy.visit('https://staging.blavity.com/entertainment/whoopi-goldberg-the-color-purple-2023-scene');
       Cypress.on('uncaught:exception', (err, runnable) => { return false; })
