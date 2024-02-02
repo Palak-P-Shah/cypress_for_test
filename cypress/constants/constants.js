3e6bface2afcd18960c7efb844280280ebc0e99d
Palak P Shah <dabbadolcha@gmail.com>
2024-02-02 11:15:13 +0530

    added film page spec file

---
+++ cypress/constants/constants.js
@@ -0,0 +1,15 @@
+export const constants = {
+    URL_STG_BLAVITY: 'https://staging.blavity.com',
+    URL_PROD_BLAVITY: 'https://www.blavity.com',
+    URL_STG_BL_ARTICLE: 'https://staging.blavity.com/poinsettia-holiday-plant-named-after-a-slaveholder-name-change-possible',
+    URL_STG_BL_CAT_ENTERTAINMENT: 'https://staging.blavity.com/entertainment',
+    URL_STG_BL_CAT_FILM: 'https://staging.blavity.com/entertainment/category/film',
+    URL_PROD_BL_CAT_FILM: 'https://www.blavity.com/entertainment/category/film',
+    URL_STG_BL_CAT_TELEVISION: 'https://staging.blavity.com/entertainment/category/television',
+    URL_STG_BL_CAT_INTERVIEWS: 'https://staging.blavity.com/categories/news/interviews-sa-originals',
+    URL_STG_BL_CAT_UNSCRIPTED: 'https://staging.blavity.com/entertainment/category/unscripted',
+    URL_STG_BL_CAT_CULTURE: 'https://staging.blavity.com/categories/culture',
+    URL_STG_BL_CAT_SMALL_BUSINESS: 'https://staging.blavity.com/off-the-shelves',
+    URL_STG_BL_CAT_BLAVITY_U: 'https://staging.blavity.com/blavity-u',
+    URL_STG_AFROTECH: 'https://staging.afrotech.com'
+  }
