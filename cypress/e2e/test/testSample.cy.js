3b66fcc12b17f7bab3e799b2508174b13ae1e3ac
Palak P Shah <dabbadolcha@gmail.com>
2024-02-15 18:17:33 +0530

    chk sample file 1

--- cypress/e2e/test/testSample.cy.js
+++ cypress/e2e/test/testSample.cy.js
@@ -1,5 +1,5 @@
-describe('Test Suite to check Social Media Icons on Article page of ST BL', () => {
-    it("check if 4 Social Media Links are there or not below the Title", () => {
+describe('Social Media Icons Test', () => {
+    it("check if all soial media icons are present and clickable", () => {
         // Set viewport size to emulate a mobile resolution
         cy.viewport(375, 812);
         // Visit the URL
@@ -7,48 +7,15 @@ describe('Test Suite to check Social Media Icons on Article page of ST BL', () =
         Cypress.on("uncaught:exception", (err, runnable) => {
           return false;
         });
-        cy.get('[class="mb-10 lg:flex lg:gap-16 lg:items-center lg:mb-14"]')
-        .find('ul li a')
-        .its('length').should('eq', 4)
-    })
-    it("check if Facebook link is present and is clickable", () => {
-        cy.viewport(375, 812);
-        cy.get('[class="mb-10 lg:flex lg:gap-16 lg:items-center lg:mb-14"]')
-        .find('ul li a')
-        .eq(0)
-        .invoke('removeAttr', 'target').click({force:true})
-        cy.wait(3000)
-        cy.title().should('include', 'Facebook')
-        cy.go('back')
-    })
-    it("check if Twitter Link is present and is clickable", () => {
-        cy.viewport(375, 812);
-        cy.get('[class="mb-10 lg:flex lg:gap-16 lg:items-center lg:mb-14"]')
-        .find('ul li a')
-        .eq(1)
-        .invoke('removeAttr', 'target').click({force:true})
-        cy.wait(3000)
-        cy.title().should('include', 'X')
-        cy.go('back')
-    })
-    it("check if LinkedIn Link is present and is clickable", () => {    
-        cy.viewport(375, 812);
-        cy.get('[class="mb-10 lg:flex lg:gap-16 lg:items-center lg:mb-14"]')
-        .find('ul li a')
-        .eq(2)
-        .invoke('removeAttr', 'target').click({force:true})
-        cy.wait(3000)
-        cy.title().should('include', 'LinkedIn')
-        cy.go('back')
-    })
-    it("check if Threads Link is present and is clickable", () => {    
-        cy.viewport(375, 812);
-        cy.get('[class="mb-10 lg:flex lg:gap-16 lg:items-center lg:mb-14"]')
-        .find('ul li a')
-        .eq(3)
-        .invoke('removeAttr', 'target').click({force:true})
-        cy.wait(3000)
-        cy.title().should('include', 'Threads')
-        // cy.go('back')
-      })
+          cy.get('[class="flex gap-6 items-center"] li a').each(($socialIcon, index) => {
+            // Get the href attribute of the social media icon
+            const href = $socialIcon.attr('href');
+            // Open the social media icon in a new tab
+            cy.visit(href, { timeout: 80000 });
+            // Log a message to indicate that the social media icon was visited
+            cy.log(`Visited social media icon ${index + 1}`);
+            // Switch back to the original page
+            cy.go('back');
+          });
+      });
   });
