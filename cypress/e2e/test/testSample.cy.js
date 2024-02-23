6da8d12e7743d28f1cb73900bcea76a9e4f8f2a5
Palak P Shah <dabbadolcha@gmail.com>
2024-02-23 21:44:07 +0530

    sample file for newsletter signup in article page ST BL 1

--- cypress/e2e/test/testSample.cy.js
+++ cypress/e2e/test/testSample.cy.js
@@ -1,155 +1,61 @@
 import { constants } from "../../constants";
-const categoryUrls = [
-                        constants.URL_STG_BL_CAT_ENT_FILM,
-                        constants.URL_STG_BL_CAT_ENT_TELEVISION,
-                        //constants.URL_STG_BL_CAT_ENT_UNSCRIPTED,
-                        //constants.URL_STG_BL_CAT_ENT_INTERVIEWS,                        
-                     ]
-    describe('Test each category page of Entertainment [SA] like Film, Television', () => 
-    {
-        categoryUrls.forEach(categoryUrl => 
+describe('Test Suite to check whether colon is present above the video in article page' , function() {
+  
+    it('should sign-up with email section 1 within any article page within Right Rail', () =>{
+        cy.viewport(1920, 1080);
+        cy.visit(constants.URL_STG_BLAVITY)
+        Cypress.on('uncaught:exception', (err, runnable) => { return false; });
+        cy.get('[class="heading-h1 mb-4 text-neutral-black lg:mb-0"]')
+ 
+        .scrollIntoView()
+        .click({force: true})
+        cy.wait(7000)
+        
+        cy.fixture('email').then(function(email)
         {
-
-                // beforeEach(() => {
-                //     })
-
-            it(`tests url ${categoryUrl}`, () => 
-            {
-                cy.viewport(1920, 1080);
-                cy.visit(categoryUrl)
-                Cypress.on('uncaught:exception', (err, runnable) => { return false; })  
-                // Cypress.on('uncaught:exception', (err, runnable) => { return false; })
-                cy.url().then(($url) => {
-                    if($url.includes("film")) 
-                    {
-                        cy.title().should('eq','Film - Blavity News')
-                        
-                    }
-                    if($url.includes("television")) 
-                    {
-                        cy.title().should('eq','Television - Blavity News')
-                    }
-                    
-                })
-            })
-            it('Verify whether NavBar, SA Navbar and Explore SA Navbar are present or not', () => {
-                cy.viewport(1920, 1080);
-                // cy.visit(categoryUrl)
-                Cypress.on('uncaught:exception', (err, runnable) => { return false; })  
-                cy.get('[class="flex gap-x-6 items-center justify-between"]').should('exist');
-                cy.get('[class="flex gap-x-6 items-center justify-between"]').should('be.visible');
-                cy.get('[class="px-4 py-6 lg:flex lg:pr-6"]').should('exist');
-                cy.get('[class="px-4 py-6 lg:flex lg:pr-6"]').should('be.visible');
-                cy.get('[class="flex flex-col gap-y-4 lg:flex-row lg:gap-x-10 lg:items-center"]').should('exist');
-                cy.get('[class="flex flex-col gap-y-4 lg:flex-row lg:gap-x-10 lg:items-center"]').should('be.visible');
-            })
-            it('Verify whether Heading is present or not', () => {
-                cy.viewport(1920, 1080);
-                // cy.visit(categoryUrl)
-                Cypress.on('uncaught:exception', (err, runnable) => { return false; })  
-                cy.get('[class="heading-h1 mb-16"]').should('exist');
-                cy.get('[class="heading-h1 mb-16"]').should('be.visible');
-                // cy.url().then(($url) => {
-                //     if($url.includes("film")) 
-                //     {
-                //         cy.get('[class="heading-h1 mb-16"]')
-                //         .should('eq','Film')    
-                //     }
-                // })
-            })
-            it('get Number of Stories in Category page', () => {
-                cy.viewport(1920, 1080);
-                Cypress.on('uncaught:exception', (err, runnable) => { return false; })  
-                cy.get('[class="grid gap-10 lg:gap-8 lg:grid-cols-4"]')
-                .children()
-                .its('length')
-                .should('eq', 8)
-            })
-            it('Verify Whether Newsletter section is present above Footer in Category page', () => {
-                cy.viewport(1920, 1080);
-                Cypress.on('uncaught:exception', (err, runnable) => { return false; })  
-                cy.get('[class="py-10 text-neutral-white lg:py-16 sa-subscribe-banner-1 lg:mt-20"]')
-                .scrollIntoView()
-                .should('exist')
-                .should('be.visible')
-            })
-            it('Verify Whether Footer is present in Category page', () => {
-                cy.viewport(1920, 1080);
-                Cypress.on('uncaught:exception', (err, runnable) => { return false; })  
-                cy.get('[class="main-footer bg-neutral-black py-8 text-neutral-white lg:pb-16"]')
-                .scrollIntoView()
-                .should('exist')
-                .should('be.visible')
-            })
-            it('Verify whether Image of Each Article is present or not', () => {
-                // cy.wait(3000)
-                cy.viewport(1920, 1080);
-                Cypress.on('uncaught:exception', (err, runnable) => { return false; })  
-                cy.get('[class="aspect-[4/3] object-cover w-full"]').each(($img, index, $imgs) => {
-                    cy.wrap($img).scrollIntoView()
-                    // cy.wrap($img).click({force:true});
-                    cy.wrap($img).then(($imgElement) => {
-                        expect($imgElement[0].naturalWidth).to.be.greaterThan(0);
-                    });
-                    cy.wrap($img).then(($imgElement) => {
-                        expect($imgElement[0].naturalHeight).to.be.greaterThan(0);
-                    });
-              });
-            })
-            it('Verify whether title of Each Article is present or not', () => {
-                // cy.wait(3000)
-                cy.viewport(1920, 1080);
-                Cypress.on('uncaught:exception', (err, runnable) => { return false; })  
-                cy.get('[class="line-clamp-2"]').each(($title, $index, $titles) => {
-                    cy.wrap($title)
-                    .should('exist')
-                    .should('be.visible')
-                    cy.wrap($title).scrollIntoView()
-                    cy.wrap($title).its('length').should('be.gt', 0)
-                    // cy.wrap($title).click({force: true})
-                })
-            })
-            it('Verify whether tags of Each Article are present or not', () => {
-                cy.wait(3000)
-                cy.viewport(1920, 1080);
-                Cypress.on('uncaught:exception', (err, runnable) => { return false; })  
-                cy.get('[class="flex flex-wrap items-center mt-2 lg:flex-1"]')
-                .find('a')
-                .each(($tag, $index, $tags) => {
-                    cy.wrap($tag)
-                    .should('exist')
-                    .should('be.visible')
-                    cy.wrap($tag).scrollIntoView()
-                    cy.wrap($tag).its('length').should('be.gt', 0)
-                    // cy.wrap($tag).click({force: true})
-                })
-            })
-            it('Verify whether Published Date of Each Article are present or not', () => {
-                cy.wait(3000)
-                cy.viewport(1920, 1080);
-                Cypress.on('uncaught:exception', (err, runnable) => { return false; })  
-                cy.get('[class="flex flex-wrap items-center mt-2 lg:flex-1"]')
-                .find('p')
-                .each(($date, $index, $dates) => {
-                    cy.wrap($date)
-                    .should('exist')
-                    .should('be.visible')
-                    cy.wrap($date).scrollIntoView()
-                    cy.wrap($date).its('length').should('be.gt', 0)
-                    // cy.wrap($tag).click({force: true})
-                })
-            })
-            it('get Number of Stories in Category page after clicking load more', () => {
-                cy.viewport(1920, 1080);
-                Cypress.on('uncaught:exception', (err, runnable) => { return false; })  
-                cy.get('[class="button-link border border-main-hornblende-green max-w-[198px] py-[13px] w-full"]')
-                .scrollIntoView()
-                .click({force:true})
-                cy.wait(2000)
-                cy.get('[class="grid gap-10 lg:gap-8 lg:grid-cols-4"]')
-                .children()
-                .its('length')
-                .should('eq', 16)
-              })
+            Cypress.on('uncaught:exception', (err, runnable) => { return false; });
+            this.email=email;
+          
+            cy.get('[class="focus-within:outline-none paragraph-2 bg-main-citron border-b border-main-hornblende-green mx-4 py-3"]')
+            .type('email')
+            .scrollIntoView()
+            .click({force: true})
+            
+            .type(this.email.email)
+            cy.get('[class="button-link cursor-pointer bg-main-hornblende-green mb-4 mx-4 px-4 py-3.5 text-neutral-white"]')
+            // .click()
+            .click({force: true})
+            cy.wait(4000)
+            cy.get('[class="heading-h3 mb-8 mx-auto"]')
+            .should('be.visible')
+            .should('contain', ' Thank you for subscribing! ')
+        })    
+    })   
+    it('should sign-up with email section 2 within any article page, after "You May Like" section', () =>{
+        cy.viewport(1920, 1080);
+        
+        Cypress.on('uncaught:exception', (err, runnable) => { return false; });
+        cy.fixture('email').then(function(email)
+        {
+            Cypress.on('uncaught:exception', (err, runnable) => { return false; });
+            this.email=email;
+      
+            cy.get('[class="focus-within:outline-none"]')
+            .first()
+            .scrollIntoView()
+          
+            .click({force: true})
+           
+            .type(this.email.email)
+            cy.get('[class="button-link cursor-pointer"]')
+            .first()
+            .click({force: true})
+            cy.wait(4000)
+            cy.get('[class="subscribe-category-selector__form flex flex-col gap-y-4 lg:flex-row lg:flex-wrap lg:gap-x-0.5 lg:gap-y-2"]')
+            .last()
+            .should('be.visible')
+            .should('contain', ' Thank you for subscribing! ')
+            
+        })
     })
 })
